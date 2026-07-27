1554bc99af49ad5ac04639f53f2b1b647d987305
kasthuri1234-art <kasthurimanjunath895@gmail.com>
2026-07-27 23:03:03 +0530

    Add files via upload

---
+++ UserInput.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class UserInput {
+    public static void main(String[] args) {
+        Scanner pb = new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int num = pb.nextInt();
+        System.out.println("enterde" + num);
+    }
+}
